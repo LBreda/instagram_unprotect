07aca16e5cb43f806c921276fc1fdcf064459169
Lorenzo Breda <lorenzo@lbreda.com>
2023-03-23 23:09:05 +0100

    :sparkles: Support for user pages

--- Instagram_unprotect.meta.js
+++ Instagram_unprotect.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version    2.1
+// @version    2.2
 // @name    Instagram unprotect
 // @license      MIT
 // @namespace    http://lbreda.com/
--- Instagram_unprotect.user.js
+++ Instagram_unprotect.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Instagram unprotect
 // @namespace    http://lbreda.com/
-// @version      2.1
+// @version      2.2
 // @description  Unprotects Instagram images in the single-image pages
 // @author       Lorenzo Breda
 // @license      MIT
-// @match        https://www.instagram.com/p/*
+// @match        https://*.instagram.com/*
 // @grant        none
 // ==/UserScript==
 
 function unprotectImage() {
-    [...document.querySelectorAll('img[src*=cdninstagram][srcset*=cdninstagram]')].forEach((image) => {
+    [...document.querySelectorAll('._aagv img')].forEach((image) => {
         let imgContainer = image?.parentNode.parentNode;
         if(imgContainer?.getElementsByTagName('div')[1]) imgContainer?.removeChild(imgContainer?.getElementsByTagName('div')[1]);
     })
@@ -20,11 +20,28 @@ function callback(mutationList, observer) {
     unprotectImage();
 }
 
+function attachObserverToArticle() {
+    let waitForArticle = setInterval(() => {
+        if (document.querySelector('body')) {
+            (new MutationObserver(callback)).observe(document.querySelector('body'), { attributes: false, childList: true, subtree: true });
+            clearInterval(waitForArticle);
+        }
+    }, 20)
+}
+
 (function() {
     'use strict';
 
-    window.addEventListener('load', function() {
-        (new MutationObserver(callback)).observe(document.querySelector('body'), { attributes: false, childList: true, subtree: true });
-    }, false);
+    let currentURL = location.href;
+    let firstCycle = true;
 
+    let mainCycle = setInterval(() => {
+        if(currentURL != location.href || firstCycle) {
+            currentURL = location.href;
+            firstCycle = false;
+            if(currentURL.startsWith("https://www.instagram.com/p/")) {
+               attachObserverToArticle();
+            }
+        }
+    }, 20)
 })();
